c02b82ea9764e02f205638d1ffb7699a1960c788
Libitum <libitum@msn.com>
2019-09-08 11:43:24 +0800

    init the project

---
+++ Jellyfin.Plugin.Douban/Plugin.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Jellyfin.Plugin.Douban.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.Douban
+{
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public override string Name => "Douban";
+        public override Guid Id => Guid.Parse("e325b8d5-5f54-447f-a38a-a951b933d22c");
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public static Plugin Instance { get; private set; }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = this.Name,
+                    EmbeddedResourcePath = string.Format("{0}.Configuration.configPage.html", GetType().Namespace)
+                }
+            };
+        }
+    }
+}
