4c85bbb8c16a211bee24b961f469b69e4251cea5
Libitum <libitum@msn.com>
2020-05-29 21:09:04 +0800

    Support fetching background

---
+++ Jellyfin.Plugin.Douban.Tests/ImageProviderTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+using Jellyfin.Plugin.Douban.Tests.Mock;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Jellyfin.Plugin.Douban.Tests
+{
+    public class ImageProviderTest
+    {
+        private readonly ImageProvider _imageProvider;
+        private readonly IServiceProvider _serviceProvider;
+
+        public ImageProviderTest()
+        {
+            _serviceProvider = new ServiceCollection().AddLogging(builder => builder.AddConsole())
+                                                      .BuildServiceProvider();
+            var loggerFactory = _serviceProvider.GetService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("test");
+
+            var httpClient = new MockHttpClient();
+            var jsonSerializer = new MockJsonSerializer();
+            _imageProvider = new ImageProvider(httpClient, jsonSerializer, logger);
+        }
+
+        [Fact]
+        public void TestGetBackdrop()
+        {
+            // Test 1:
+            var list = _imageProvider.GetBackdrop("5350027", CancellationToken.None).Result;
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Url);
+                Console.WriteLine(item.Type);
+            }
+            Assert.Single(list);
+        }
+    }
+}
--- Jellyfin.Plugin.Douban/ImageProvider.cs
+++ Jellyfin.Plugin.Douban/ImageProvider.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -39,12 +42,11 @@ namespace Jellyfin.Plugin.Douban
                     "is empty: {Name}", item.Name);
                 return list;
             }
-            var movie = await GetDoubanSubject(sid, cancellationToken);
-            list.Add(new RemoteImageInfo
-            {
-                ProviderName = Name,
-                Url = movie.Images.Large,
-            });
+            var primaryList = await GetPrimary(sid, cancellationToken);
+            var backdropList = await GetBackdrop(sid, cancellationToken);
+            list.AddRange(primaryList);
+            list.AddRange(backdropList);
+
             return list;
         }
 
@@ -57,8 +59,68 @@ namespace Jellyfin.Plugin.Douban
         {
             return new List<ImageType>
             {
-                ImageType.Primary
+                ImageType.Primary,
+                ImageType.Backdrop
             };
+        }
+
+        public async Task<IEnumerable<RemoteImageInfo>> GetPrimary(string sid,
+            CancellationToken cancellationToken)
+        {
+            var list = new List<RemoteImageInfo>();
+            var movie = await GetDoubanSubject(sid, cancellationToken);
+            list.Add(new RemoteImageInfo
+            {
+                ProviderName = Name,
+                Url = movie.Images.Large,
+                Type = ImageType.Primary
+            });
+            return list;
+        }
+        public async Task<IEnumerable<RemoteImageInfo>> GetBackdrop(string sid,
+            CancellationToken cancellationToken)
+        {
+            var url = string.Format("https://movie.douban.com/subject/{0}/photos?" +
+                                    "type=W&start=0&sortby=size&size=a&subtype=a", sid);
+
+            var options = new HttpRequestOptions
+            {
+                Url = url,
+                CancellationToken = cancellationToken,
+                BufferContent = true,
+                EnableDefaultUserAgent = true,
+            };
+
+            using var response = await _httpClient.GetResponse(options).
+                    ConfigureAwait(false);
+            using var reader = new StreamReader(response.Content);
+            String content = reader.ReadToEnd();
+            const String pattern = @"(?s)data-id=""(\d+)"".*?class=""prop"">\n\s*(\d+)x(\d+)";
+            Match match = Regex.Match(content, pattern);
+
+            var list = new List<RemoteImageInfo>();
+            while (match.Success)
+            {
+                string data_id = match.Groups[1].Value;
+                string width = match.Groups[2].Value;
+                string height = match.Groups[3].Value;
+                _logger.LogDebug("Find backdrip id {0}, size {1}x{2}", data_id, width, height);
+
+                if (float.Parse(width) > float.Parse(height) * 1.3)
+                {
+                    // Just chose the Backdrop which width is larger than height
+                    list.Add(new RemoteImageInfo
+                    {
+                        ProviderName = Name,
+                        Url = string.Format("https://img9.doubanio.com/view/photo/l/public/p{0}.webp", data_id),
+                        Type = ImageType.Backdrop,
+                    });
+                }
+
+                match = match.NextMatch();
+            }
+
+            return list;
         }
     }
 }
