b8c20581676ddd8a9a56cde9e3e1b313896c9b60
Libitum <libitum@msn.com>
2020-06-29 16:21:20 +0800

    Use random user-agent

--- Jellyfin.Plugin.Douban/DoubanAccessor.cs
+++ Jellyfin.Plugin.Douban/DoubanAccessor.cs
@@ -14,16 +14,17 @@ namespace Jellyfin.Plugin.Douban
         private readonly IHttpClient _httpClient;
         private readonly ILogger _logger;
 
-        private readonly Random _random;
+        private static readonly Random _random = new Random();
         // It's used to store the last access time, to reduce the access frequency.
-        private static long _lastAccessTime;
-        private static SemaphoreSlim _locker = new SemaphoreSlim(1, 1);
-
+        private static long _lastAccessTime = 0;
+        
         // It's used to store the value of BID in cookie.
-        private string _doubanBid;
+        private static string _doubanBid = "";
+
+        private static readonly SemaphoreSlim _locker = new SemaphoreSlim(1, 1);
 
         // Used as the user agent when access Douban.
-        private readonly string[] _userAgentList = {
+        private static readonly string[] _userAgentList = {
             "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
             "Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20100101 Firefox/19.0",
             "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36",
@@ -37,12 +38,12 @@ namespace Jellyfin.Plugin.Douban
             "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1467.0 Safari/537.36"
         };
 
+        private static readonly string _userAgent = _userAgentList[_random.Next(_userAgentList.Length)];
+
         public DoubanAccessor(IHttpClient client, ILogger logger)
         {
             _httpClient = client;
             _logger = logger;
-            _lastAccessTime = 0;
-            _random = new Random();
         }
 
         public async Task<String> GetResponse(string url, CancellationToken cancellationToken)
@@ -52,8 +53,7 @@ namespace Jellyfin.Plugin.Douban
                 Url = url,
                 CancellationToken = cancellationToken,
                 BufferContent = true,
-                //UserAgent = _userAgentList[_random.Next(_userAgentList.Length)],
-                UserAgent = _userAgentList[0],
+                UserAgent = _userAgent,
             };
 
             if (!string.IsNullOrEmpty(_doubanBid))
@@ -63,22 +63,18 @@ namespace Jellyfin.Plugin.Douban
 
             using var response = await _httpClient.GetResponse(options).ConfigureAwait(false);
 
-            // For test
-            foreach (var h in response.Headers)
-            {
-                _logger.LogError("Douban header key:{0}", h.Key);
-            }
-
             if (response.Headers.TryGetValues("X-DOUBAN-NEWBID", out IEnumerable<string> value))
             {
-                _logger.LogInformation("Douban bid is: {0}", value.FirstOrDefault());
-                _doubanBid = value.FirstOrDefault();
+                lock (_doubanBid)
+                {
+                    _logger.LogInformation("Douban bid is: {0}", value.FirstOrDefault());
+                    _doubanBid = value.FirstOrDefault();
+                }
             }
 
             using var reader = new StreamReader(response.Content);
             String content = reader.ReadToEnd();
 
-            
             return content;
         }
 
