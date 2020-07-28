59571af49fbafb2ea9ecb03f053840eecf2ac46c
Libitum <libitum@msn.com>
2020-07-28 20:06:52 +0800

    Make the plugin compatible with Jellyfin 10.6.0

--- Jellyfin.Plugin.Douban/ExternalID.cs
+++ Jellyfin.Plugin.Douban/ExternalID.cs
@@ -2,14 +2,17 @@ using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Providers;
 
 namespace Jellyfin.Plugin.Douban
 {
     public class DoubanExternalId : IExternalId
     {
-        public string Name => "Douban";
+        public string ProviderName => "Douban";
 
         public string Key => BaseProvider.ProviderID;
+
+        public ExternalIdMediaType? Type => null;
 
         public string UrlFormatString => "https://movie.douban.com/subject/{0}/";
 
